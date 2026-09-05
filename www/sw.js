7b39dc6e09e0350bba456174e557686beb3df0c7
Jordi <j.vidiellamposta@gmail.com>
2026-09-05 13:02:30 +0200

    Add files via upload

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mpt-v3_71';
+const CACHE = 'mpt-v3_72';
 const FILES = ['./', './index.html', './i18n.js', './contenido.js', './practices.js', './estilos.css', './noveno-es.png', './noveno-en.png', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
